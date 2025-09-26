08fb144f987d20f620c7836f77b2de8b315e6696
Boris Syrpin <barneymorris.us@gmail.com>
2025-09-27 01:04:57 +0300

    Note status and repository

---
+++ src/main/java/notes/app/services/impl/NoteServiceImpl.java
@@ -0,0 +1,41 @@
+package notes.app.services.impl;
+
+import lombok.RequiredArgsConstructor;
+import notes.app.models.Note;
+import notes.app.repository.NoteRepository;
+import notes.app.services.NoteService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class NoteServiceImpl implements NoteService {
+    private final NoteRepository noteRepository;
+
+    @Override
+    public Note createNoteForUser(String username, String content) {
+        Note note = new Note();
+        note.setContent(content);
+        note.setOwnerUsername(username);
+        return noteRepository.save(note);
+    }
+
+    @Override
+    public Note updateNoteForUser(Long noteId, String content, String username) {
+        Note note = noteRepository.findById(noteId).orElseThrow(() -> new RuntimeException("Note not found with id: " + noteId));
+        note.setContent(content);
+        Note updatedNote = noteRepository.save(note);
+        return note;
+    }
+
+    @Override
+    public void deleteNoteForUser(Long noteId, String username) {
+        noteRepository.deleteById(noteId);
+    }
+
+    @Override
+    public List<Note> getNotesForUser(String username) {
+        return noteRepository.findByOwnerUsername(username);
+    }
+}
