0148d7bd7a06d114ea47f9fbb12cd71c0bae22e0
Boris Syrpin <barneymorris.us@gmail.com>
2025-09-27 22:45:34 +0300

    update security config

--- src/main/java/notes/app/security/SecurityConfig.java
+++ src/main/java/notes/app/security/SecurityConfig.java
@@ -5,11 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,31 +14,9 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) ->
-            requests
-                    .requestMatchers("/contact").permitAll()
-                    .requestMatchers("/public/**").permitAll()
-                    .requestMatchers("/admin").denyAll()
-                    .requestMatchers("/admin/**").denyAll()
-                    .anyRequest().authenticated());
-
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
         http.csrf(AbstractHttpConfigurer::disable);
-        http.sessionManagement(
-                session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS
-                        )
-        );
         http.httpBasic(withDefaults());
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password("{noop}password") // {noop} чтобы без шифрования
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(admin);
     }
 }
