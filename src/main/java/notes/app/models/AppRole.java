fb523d266a0c2413631a4dddaa9d22dc0448c0c7
Boris Syrpin <barneymorris.us@gmail.com>
2025-09-29 21:50:36 +0300

    user, roles repos, models and so on

---
+++ src/main/java/notes/app/models/AppRole.java
@@ -0,0 +1,6 @@
+package notes.app.models;
+
+public enum AppRole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/notes/app/models/Role.java
@@ -0,0 +1,36 @@
+package notes.app.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ToString.Exclude
+    @Enumerated
+    @Column(length = 20, name = "role_name")
+    private AppRole roleName;
+
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    @JsonBackReference
+    @ToString.Exclude
+    private Set<User> users = new HashSet<>();
+
+    public Role(AppRole roleName) {
+        this.roleName = roleName;
+    }
+}
