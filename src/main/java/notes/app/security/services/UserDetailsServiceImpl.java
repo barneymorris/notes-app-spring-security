781f1b46fb1e9925a394625c0761d925446fdd80
Boris Syrpin <barneymorris.us@gmail.com>
2025-09-29 22:20:34 +0300

    custom user details service

---
+++ src/main/java/notes/app/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package notes.app.security.services;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import notes.app.models.User;
+import notes.app.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
