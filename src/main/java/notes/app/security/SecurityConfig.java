d456f8c03bd5161b29185b2acf1ac44834628a4c
Boris Syrpin <barneymorris.us@gmail.com>
2025-09-29 20:42:05 +0300

    jdbc user details manager

--- src/main/java/notes/app/security/SecurityConfig.java
+++ src/main/java/notes/app/security/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -13,10 +17,35 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity()
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
         http.csrf(AbstractHttpConfigurer::disable);
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager();
+
+        if (!manager.userExists("user1")) {
+            manager.createUser(
+                    User.withUsername("user1")
+                            .password("{noop}password1")
+                            .roles("USER")
+                            .build()
+            );
+        }
+
+        if (!manager.userExists("admin")) {
+            manager.createUser(
+                    User.withUsername("admin")
+                            .password("{noop}adminPass")
+                            .roles("ADMIN")
+                            .build()
+            );
+        }
+
+        return manager;
     }
 }
