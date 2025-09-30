9e4aacb66fc96cc293ad9d1637a05a887f44abf5
Boris Syrpin <barneymorris.us@gmail.com>
2025-09-30 23:42:52 +0300

    custom logging filter

--- src/main/java/notes/app/security/SecurityConfig.java
+++ src/main/java/notes/app/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -28,10 +29,11 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http, CustomLoggingFilter customLoggingFilter) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
         http.csrf(AbstractHttpConfigurer::disable);
         http.httpBasic(withDefaults());
+        http.addFilterBefore(customLoggingFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
