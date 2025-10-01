1ec50f2a406fb46c08ad7705ad0f4ea5eb6176b6
Boris Syrpin <barneymorris.us@gmail.com>
2025-10-01 19:58:16 +0300

    signup request and others

---
+++ src/main/java/notes/app/security/response/MessageResponse.java
@@ -0,0 +1,14 @@
+package notes.app.security.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+}
